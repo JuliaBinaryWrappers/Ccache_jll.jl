ce8a2d800a1a6f5adcf3e997082f2d4d038ac417
jlbuild <juliabuildbot@gmail.com>
2022-11-23 23:50:14 +0000

    Ccache_jll build 4.7.4+0

--- src/wrappers/powerpc64le-linux-gnu-cxx11.jl
+++ src/wrappers/powerpc64le-linux-gnu-cxx11.jl
@@ -3,10 +3,11 @@ export ccache
 
 using Zlib_jll
 using Zstd_jll
+using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("Ccache")
 JLLWrappers.@declare_executable_product(ccache)
 function __init__()
-    JLLWrappers.@generate_init_header(Zlib_jll, Zstd_jll)
+    JLLWrappers.@generate_init_header(Zlib_jll, Zstd_jll, CompilerSupportLibraries_jll)
     JLLWrappers.@init_executable_product(
         ccache,
         "bin/ccache",
